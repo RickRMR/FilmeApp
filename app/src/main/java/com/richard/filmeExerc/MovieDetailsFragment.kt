ab7896b2dcf6122bfbcc3dbbe58c16b4ff327571
RickRMR <148383472+RickRMR@users.noreply.github.com>
2025-08-20 09:33:27 -0300

    teste
    teste

--- app/src/main/java/com/richard/filmeExerc/MovieDetailsFragment.kt
+++ app/src/main/java/com/richard/filmeExerc/MovieDetailsFragment.kt
@@ -1,12 +1,10 @@
 package com.richard.filmeExerc
 
-import android.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,9 +32,9 @@ class HQDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.id.)
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
     }
 
     companion object {
